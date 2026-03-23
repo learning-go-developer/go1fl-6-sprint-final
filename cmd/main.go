71c487ca1630104cf1c0e0c2654b92422a225e93
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-03-23 11:13:41 -0400

    feat: implement main package

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("Starting server on :8080...")
+
+	if err := srv.HttpSrv.ListenAndServe(); err != nil {
+		logger.Fatal("Server failed to start: ", err)
+	}
 }
